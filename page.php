de0b0ae75046e16b5c9bead55f87dde7128dbe06
Manonthemon <klimowicz.maciej@gmail.com>
2023-08-16 15:54:34 +0100

    Added dynamic breadcrumbs

--- page.php
+++ page.php
@@ -1,29 +1,35 @@
-
-<?php 
+<?php
 
 get_header();
 
 while (have_posts()) {
-the_post(); ?> 
+    the_post(); ?>
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title() ?></h1>
-        <div class="page-banner__intro">
-          <p>!!!!!REPLACE WITH PHP in PAGE.PHP LATER!!!!!!!</p>
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+            style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+                <?php the_title() ?>
+            </h1>
+            <div class="page-banner__intro">
+                <p>!!!!!REPLACE WITH PHP in PAGE.PHP LATER!!!!!!!</p>
+            </div>
         </div>
-      </div>
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
+        <?php
+        if (wp_get_post_parent_id(get_the_ID())) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">
+                        <?php the_title() ?></span></p>
+            </div>
+ <?php
+        }
+        ?>
 
-      <!-- <div class="page-links">
+            <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
@@ -31,14 +37,14 @@ the_post(); ?>
         </ul>
       </div> -->
 
-      <div class="generic-content">
-       <?php the_content();?>
-      </div>
-    </div>
+            <div class="generic-content">
+                <?php the_content(); ?>
+            </div>
+        </div>
 
 
 
-<?php }
+    <?php }
 
 get_footer();
 
