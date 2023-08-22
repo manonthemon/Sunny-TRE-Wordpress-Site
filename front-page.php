aa0aab9d05fab8c1490aa6023572d0c0c75e5528
Manonthemon <klimowicz.maciej@gmail.com>
2023-08-22 16:27:42 +0100

    Added archives

--- front-page.php
+++ front-page.php
@@ -41,7 +41,16 @@
       </div>
       <div class="full-width-split__two">
         <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+          <h2 class="headline headline--small-plus t-center">From The Blog</h2>
+          <?php 
+$homepagePosts = new WP_Query();
+
+          while(have_posts()){
+the_post(); ?>
+<li><?php the_title(); ?></li>
+
+        <?php  }
+          ?>
 
           <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
