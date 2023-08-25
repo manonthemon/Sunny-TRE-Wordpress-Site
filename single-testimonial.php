93fcf322c0a9be20f00a0e28cf963f769aff23ec
Manonthemon <klimowicz.maciej@gmail.com>
2023-08-25 09:46:38 +0100

    Updated testimonials posts

--- single-testimonial.php
+++ single-testimonial.php
@@ -19,12 +19,7 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('testimonial'); ?>">
-                    <i class="fa fa-home" aria-hidden="true"></i> Testimonials Home </a><span class="metabox__main"><?php the_title();?>
-                </span></p>
-        </div>
-
+        
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
