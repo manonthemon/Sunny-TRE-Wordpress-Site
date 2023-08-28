855e15ad8183d8fc21ecd0f47f3da75537327f95
Manonthemon <klimowicz.maciej@gmail.com>
2023-08-28 14:36:34 +0100

    Added custom background images for pages, made a fuctnion handling images, titles and subtitles

--- functions.php
+++ functions.php
@@ -1,4 +1,41 @@
 <?php 
+
+function pageBanner($args = array()) {
+    
+    // Provide default values for 'title' and 'subtitle' if they are missing in $args
+    $title = isset($args['title']) ? $args['title'] : get_the_title();
+    $subtitle = isset($args['subtitle']) ? $args['subtitle'] : get_field('page_banner_subtitle');
+    
+    // Provide a default photo URL if 'photo' is missing in $args and custom field is not set
+    if (isset($args['photo'])) {
+        $photo_url = $args['photo'];
+    } elseif (get_field('page_banner_background_image')) {
+        $photo_url = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+        $photo_url = get_theme_file_uri('/images/ocean.jpg');
+    }
+
+    // Output the HTML
+    ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo $photo_url; ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+                <?php echo $title; ?>
+            </h1>
+            <div class="page-banner__intro">
+                <p><?php echo $subtitle; ?></p>
+            </div>
+        </div>
+    </div>
+    <?php
+    // End HTML output
+}
+
+
+
+
 function sunny_files() {
     wp_enqueue_script('main_sunny_js' , get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true );
     wp_enqueue_style('google_fonts' , '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -16,6 +53,7 @@ function sunny_features () {
     add_theme_support('post-thumbnails');
     add_image_size('serviceLandscape', 400, 260, true);
     add_image_size('servicePortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 add_action( 'after_setup_theme', 'sunny_features');
 
