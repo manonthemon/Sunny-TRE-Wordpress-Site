babdb7867561c8cbd89779e1aeeee3db7cfa4687
Manonthemon <klimowicz.maciej@gmail.com>
2023-09-13 22:51:29 +0100

    Bug fixes

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -22,12 +22,12 @@
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
             <p>
               <?php  if (has_excerpt()) {
-              echo wp_trim_words(get_the_excerpt(), 55); // Limit excerpt to 20 words
+              echo wp_trim_words(get_the_excerpt(),35); // Limit excerpt to 20 words
             } else {
-              echo wp_trim_words(get_the_content(), 55); // If no excerpt, limit content to 20 words
+              echo wp_trim_words(get_the_content(), 35); // If no excerpt, limit content to 20 words
             }
               ?> 
-              <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">View event &raquo;</a></p>
+              <!-- <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">View event &raquo;</a></p> -->
             </p>
           </div>
         </div>
